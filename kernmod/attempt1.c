fef6e650bed994aa25dd8261eaf2ed5c125bdd3b
alexreuter <areuter12003310@gmail.com>
2021-04-23 15:07:12 -0400

    Cleaned code

--- kernmod/attempt1.c
+++ kernmod/attempt1.c
@@ -1,17 +1,3 @@
-/*
- * kernread
- *
- * emulates a kernel vulnerability that allows an attacker
- * to overwrite a kernel-mapped data pointer with an arbitrary
- * value. It was inspired by Nelson Elhage's NULL pointer
- * dereference module (NULLDEREF)
- *
- * Usage:
- * 	- compile and load the module
- * 	- mount debugfs (mount -t debugfs none /sys/kernel/debug/)
- *	- go to /sys/kernel/debug/kernread/ and have fun :)
- */
-
 #include <linux/debugfs.h>
 #include <linux/hardirq.h>
 #include <linux/init.h>
@@ -41,20 +27,6 @@ struct datapoints{
 
 struct datapoints myData;
 
-// const size_t mem_size = 1 << 20;
-//first success
-
-//const size_t mem_size = 1 << 20;
-//const size_t mem_size = 536870912 * 2;
-//const size_t mem_size = 1<<7;
-//const size_t base_addr_inc = mem_size/2;
-//const size_t base_addr_inc = mem_size/100;
-//const size_t base_addr_inc = 1 << 18;
-//2^31 is 2GB of bytes, which is the worst case scenario for a bank size
-//Should definitelly change the probe size, assuming the memory is contiguous
-//const size_t probe_inc = 1 << 8;
-//const size_t probe_inc = mem_size/1000;
-
 const size_t total_size = 1<<22;
 const size_t window_size = total_size/8;
 const size_t base_addr_inc = window_size;
@@ -70,8 +42,6 @@ unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;
 
 int inline measured_function(volatile int offset) {    
 	volatile int w, x, y, z, a, b;
-//	int i;	
-//	for(i = 0; i < 100; i++){
 	w = g_mem[base_addr_offset];
 	x = g_mem[offset];
 	asm volatile("clflush (%0)" : : "r" (g_mem + offset) : "memory");
@@ -80,26 +50,18 @@ int inline measured_function(volatile int offset) {
 	z = g_mem[offset];
 	asm volatile("clflush (%0)" : : "r" (g_mem + offset) : "memory");
     	asm volatile("clflush (%0)" : : "r" (g_mem + base_addr_offset) : "memory");
-//	}
-//    	a = g_mem[base_addr_offset];
-//	b = g_mem[offset];
-//	asm volatile("clflush (%0)" : : "r" (g_mem + offset) : "memory");
-//	asm volatile("clflush (%0)" : : "r" (g_mem + base_addr_offset) : "memory");
-//	return w + x + y + z + a + b;
+
 	return 0;
 } 
 
 static int measure_timing(int offset) {    
     unsigned long flags; 
     uint64_t start, end;    
-    //int variable = 0; 
-    //unsigned cycles_low, cycles_high, cycles_low1, cycles_high1;    
-    //printk(KERN_INFO "Loading test module...\n");    
+   
 
     preempt_disable(); /*we disable preemption on our CPU*/    
     raw_local_irq_save(flags); /*we disable hard interrupts on our CPU*/    /*at this stage we exclusively own the CPU*/    
     
-   // printk(KERN_INFO "\n %p", (char *) g_mem + offset);
 
     //Invalidate both cache lines
     asm volatile("clflush (%0)" : : "r" (g_mem + offset) : "memory");
@@ -117,36 +79,13 @@ static int measure_timing(int offset) {
     				"mov %%edx, %0\n\t"          
     				"mov %%eax, %1\n\t"    
     				"CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1):: "%rax", "%rbx", "%rcx", "%rdx");
-
-
-
-    // Old bencmarking system
-    // asm volatile (          
-    //     "RDTSC\n\t"          
-    //     "mov %%edx, %0\n\t"          
-    //     "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low));   
-
-    // measured_function(offset);   
-
-
-    // asm volatile (          
-    //     "RDTSC\n\t"          
-    //     "mov %%edx, %0\n\t"          
-    //     "mov %%eax, %1\n\t": "=r" (cycles_high1), "=r" (cycles_low1));  
+ 
 
     raw_local_irq_restore(flags); /*we enable hard interrupts on our CPU*/    
     preempt_enable();/*we enable preemption*/    
     start = ( ((uint64_t)cycles_high << 32) | cycles_low );    
     end = ( ((uint64_t)cycles_high1 << 32) | cycles_low1 );
 
-    //sprintf(myData.datapoints[entry], "%llu,p%p,v%p", (end - start), (void *) virt_to_phys((void *) g_mem + offset), (void *) (g_mem + offset));
-    
-    //Working before
-    //sprintf(myData.datapoints[entry], "%llu,p%llu\n", (end - start),  virt_to_phys((void *) g_mem + offset));
-    // entry++;
-    //end
-
-    //printk(KERN_INFO "  is %llu clock cycles, virt %p, phys %p", (end-start), (void *) g_mem + offset, (void *) virt_to_phys((void *) g_mem + offset));    
     return (int) (end - start);
 }
 
@@ -154,9 +93,7 @@ static int measure_timing(int offset) {
 static ssize_t
 read_output(struct file*f, char __user *buf, size_t count, loff_t *off){
 	int ret;
-	// printk(KERN_INFO "Got a call to read_output!\n");
 	ret = simple_read_from_buffer(buf, count, off, output_arr, 100);
-	// printk(KERN_INFO "Finished copying to user buffer!\n");
 	return ret;
 }
 
@@ -172,9 +109,7 @@ static struct dentry *dfs = NULL;
 static const struct file_operations output_ops = {
 	.read	= read_output,
 };
-//static const struct file_operations write_output_ops = {/
-//	.write	= write_to_output,
-//};
+
 
 
 /* module cleanup; remove the files and the directory */
@@ -257,27 +192,6 @@ kernread_init(void)
 	// Zero it out
 	memset(output_arr, 0, BUFSIZE);
 
-	// debugfd_create_file(name, mode, parent, data, fops)
-
-	/* create the files with the appropriate `fops' struct and perms */
-	/*
-	output	= debugfs_create_file("output",
-						//0222, //read, read, read
-						0444,
-						kernread_root,
-NULL,
-						&output_ops);
-*/
-	// write_output	= debugfs_create_file("write_output",
-	// 					//0444, //write, write, write
-	// 					0222,
-	// 					kernread_root,
-	// 					NULL,
-	// 					&write_output_ops);
-
-	/* error handling */
-//	if (output == NULL)
-//		goto out_err;
 
 	i = 0;
 	printk(KERN_INFO "Starting rowhammering!\n");
@@ -323,16 +237,13 @@ NULL,
 				// The buffer is full
 				buff_full = 1;
 			wait:
-				//printk(KERN_INFO "The buffer is full, waiting...\n");
-				//printk(KERN_INFO "First string: %s\n", myData.datapoints[0]);
+	
 				msleep(100);
-				//printk(KERN_INFO "Should exit: %d\n", should_exit);
 				if(should_exit == 1){
 					break;
 				}else if(buff_full){
 					goto wait;
 				}else{
-					//printk(KERN_INFO "Yay the user read the buffer, clearing it and continuing...\n");
 					// The user read all of the kernel data
 					// Zero out the buffer, and set entry to 0
 					memset(myblob->data, 0, sizeof(struct datapoints));
@@ -349,7 +260,7 @@ NULL,
 				}
 			}
 
-			// avg = sum / 1000;
+
 			sprintf(myData.datapoints[entry], "%d,p%llu\n", min,  virt_to_phys((void *) g_mem + base_addr_offset + i));
     			entry++;
 		}
@@ -396,5 +307,4 @@ module_exit(kernread_exit);
 /* (un)necessary crap */
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Alex Reuter");
-MODULE_DESCRIPTION("A very simple proof of concept for kernel to user communication via debgufs");
 MODULE_VERSION("2.71alpha");
